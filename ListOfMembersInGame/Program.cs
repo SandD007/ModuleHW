10886df5fcdc808b56c4e24cbab338ebd363eb53
Dmytro Kulinich <Spo1091@i.ua>
2023-03-23 12:37:27 +0200

    Add Homework

---
+++ ListOfMembersInGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ListOfMembersInGame
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var actions = new Actions();
+            actions.Start();
+        }
+    }
+}
